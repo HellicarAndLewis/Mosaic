2be06d5dbb105fb3c761a1118a56c43a7f60890e
roxlu <diederick@apollomedia.nl>
2014-09-03 16:32:40 +0200

    Added topshop app

---
+++ install/mac-clang-x86_64/bin/preprocess_left_grid.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#set -x
+d=${PWD}
+magickdir=${d}/../imagemagick/
+infile=${1}
+tile_width=${2}
+tile_height=${3}
+output_dir=${4}
+raw_mosaic_dir=${5}
+
+function log {
+    dat=$(date +%Y.%m.%d.%H.%M.%S)
+    echo "${dat}: ${1}" >> log/preprocess.log
+}
+
+# Make sure the file exists.
+if [ ! -f ${infile} ] ; then
+    log "Cannot find ${infile}"
+    exit
+fi
+
+filename=$(basename "$infile")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+grid_filepath=${output_dir}/${filename}.png
+
+# Resize 
+${magickdir}/convert ${infile} \
+    -resize ${tile_width}x${tile_height}^ \
+    -gravity center \
+    -extent ${tile_width}x${tile_height} \
+    -colors 256 \
+    PNG8:${grid_filepath}
+
+
+mv ${infile} ${raw_mosaic_dir}/${filename}.${extension}
