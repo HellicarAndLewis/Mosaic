a77f04a2d7f91413e77985ede05ff59b96c219cb
Martin Bartels <martin@apollomedia.nl>
2014-09-06 20:36:36 +0200

    downloader and server updates

--- projects/admin/apps/image_downloader/src/server.js
+++ projects/admin/apps/image_downloader/src/server.js
@@ -10,6 +10,7 @@ var Fs = require('fs-extra');
 var Request = require('request');
 var Program = require('commander');
 var Path = require('path');
+var Gm = require('gm');
 
 var ImageDownloader = new Class({
   
@@ -37,6 +38,9 @@ var ImageDownloader = new Class({
     if(Program.settings) {
       file = Path.normalize(process.cwd() + '/' + Program.settings);
     }
+    
+    // TEMP
+    Fs.remove('/Users/martinbartels/Sites/mosaic/projects/admin/apps/image_downloader/downloads/images/', function() {
 
     // Check if file exists...
     if(Fs.existsSync(file)) {
@@ -72,18 +76,50 @@ var ImageDownloader = new Class({
           if(err) {
             self.log('Users dir ' + self.settings.image_save_path_users + 'is not ok');
           }
-        });
-        
-        Fs.ensureDir(self.settings.image_save_path_tags, function(err) {
-
-          self.log('Checking tags dir ' + self.settings.image_save_path_tags);
-
-          if(err) {
-            self.log('Tags dir ' + self.settings.image_save_path_tags + 'is not ok');
-          }
           
-          self.start();
-        });
+          Fs.ensureDir(self.settings.image_save_path_tags, function(err) {
+
+            self.log('Checking tags dir ' + self.settings.image_save_path_tags);
+
+            if(err) {
+              self.log('Tags dir ' + self.settings.image_save_path_tags + 'is not ok');
+            }
+            
+            // Resize assets
+            
+            
+            self.thumbFilename = self.settings.image_overlay_thumb 
+              + '.' 
+              + self.settings.image_size_thumb.width 
+              + 'x' 
+              + self.settings.image_size_thumb.height 
+              + '.png';
+            
+            self.largeFilename = self.settings.image_overlay_large 
+              + '.' 
+              + self.settings.image_size_large.width 
+              + 'x' 
+              + self.settings.image_size_large.height 
+              + '.png';
+            
+            // .. thumb
+            Gm(self.settings.image_overlay_thumb)
+              .resize(self.settings.image_size_thumb.width, self.settings.image_size_thumb.height, '!')
+              .colors(256)
+              .write(self.thumbFilename, function(err) {
+            
+                // .. large
+                Gm(self.settings.image_overlay_large)
+                  .resize(self.settings.image_size_large.width, self.settings.image_size_large.height, '!')
+                  .write(self.largeFilename, function(err) {
+
+                    // Start downloading!
+                    self.start();
+                  });
+                
+              });
+          });  
+        }); 
       });
  
     } else {
@@ -93,7 +129,7 @@ var ImageDownloader = new Class({
       process.exit(0);
     }
 
-    
+    }); // TEMP
   }
   
   // 
@@ -127,26 +163,35 @@ var ImageDownloader = new Class({
       // No error
       if(!error) {
         
+        // Filter images
         images.filter(function(image, i) {
+          
           return (!image.locked && (image.reviewed && image.approved));
         });
-
+        
+        // Check for images
         if(images.length > 0) {
+          
+          // Use pagination
           if(Program.compare) {
+            
             self.lastModIdMin = images[images.length-1].queue_id;
           }
+        
+        // .. no images
         } else {
           
-          setTimeout(function() {
-            
-            self.lastModIdMin = 0;
-            self.lastModIdMax = 0;
-            self.start();
-          }, self.settings.request_delay);
+          // Retry  
+          self.lastModIdMin = 0;
+          self.lastModIdMax = 0;
+          self.start();
+          
           return;
         }
         
+        // Paginantion
         if(self.lastModIdMax == 0 && Program.compare) {
+          
           self.lastModIdMax = images[0].queue_id; 
         }
         
@@ -156,6 +201,7 @@ var ImageDownloader = new Class({
           if(queue.length == 0) {
             
             setTimeout(function() {
+              
               self.start();
             }, self.settings.request_delay);
             return;
@@ -163,50 +209,154 @@ var ImageDownloader = new Class({
           
           // Pop last image
           var img = queue.pop();
+          
+          // Set all paths
+          // -----------------------------------------------------------------------------
           var tmp_file = self.settings.image_tmp_path + img.media_id + '.jpg';
+          
           var dest_file_users = self.settings.image_save_path_users + img.media_id + '.jpg';
           var dest_file_tags = self.settings.image_save_path_tags + img.media_id + '.jpg';
-          var dest_file = (img.msg_type == 'tag') ? dest_file_tags : dest_file_users;
-         
-          //var file_exists = Fs.existsSync(dest_file);
-          var file_exists = Fs.existsSync(tmp_file);
+          var dest_file_users_json = self.settings.image_save_path_users_json + img.media_id + '.json';
+          var dest_file_tags_json = self.settings.image_save_path_tags_json + img.media_id + '.json';
           
+          var thumb_dir = (img.msg_type == 'tag') ? self.settings.image_save_path_tags_thumb : self.settings.image_save_path_users_thumb;
+          var thumb_file = thumb_dir + img.media_id + '.jpg';
+          
+          var large_dir = (img.msg_type == 'tag') ? self.settings.image_save_path_tags_large : self.settings.image_save_path_users_large;
+          var large_file = large_dir + img.media_id + '.jpg';
+          
+          var dest_file = (img.msg_type == 'tag') ? dest_file_tags : dest_file_users;
+          var json_file = (img.msg_type == 'tag') ? dest_file_tags_json : dest_file_users_json;
+          
+          // -----------------------------------------------------------------------------
+          
+          // Current downloaded images
+          var current_images = [];
+          if(Fs.existsSync(self.settings.images_db)) {
+            current_images = require(self.settings.images_db);
+          }
+          
+          // Reset images db
+          if(current_images.length > (self.settings.limit * 2)) {
+            current_images.shift();
+          }
+          
+          console.log(current_images.length);
+          
+          // Check if file exists
+          var file_exists = (current_images.indexOf(img.media_id) >= 0);
+       
+          // ..if not start downloading
           if(!file_exists) {
             
             // Download image
-            self.log('Trying to download ' + img.images[self.settings.image_size].url);
+            //self.log('Trying to download ' + img.images[self.settings.image_size_download].url);
             
-            self.download(img.images[self.settings.image_size].url, tmp_file, function(err) {
+            self.download(img.images[self.settings.image_size_download].url, tmp_file, function(err) {
               
               var stats = Fs.statSync(tmp_file)
               var size = stats["size"];
               
+              // Invalid image size
               if(size < 1000) {
                 
+                // Remove invalid image
                 Fs.unlink(tmp_file, function() {
+                  
                   self.log('Image size invalid. ' + size + ' bytes received, removed tmp file ' + tmp_file);
-                  dl_img(queue);
+                  
+                  // Next
+                  setTimeout(function() {
+                    dl_img(queue);
+                  }, self.settings.image_save_delay);
                 });
-                
-              } else {
               
-                // Move image from tmp to save dir
-                Fs.copy(tmp_file, dest_file);
-                self.log('Image ' + dest_file + ' saved');
-                dl_img(queue);
-                /*Fs.rename(tmp_file, dest_file, function() {
-                  self.log('Image ' + dest_file + ' saved');
-                  dl_img(queue);
-                });*/
+              
+              // Valid size
+              } else {
+                
+           
+                  // Create thumbnail
+                  Fs.ensureDir(thumb_dir, function(err) {
+     
+                    // Image processing thumb
+                    var ix = self.settings.image_size_thumb.inner.x;
+                    var iy = self.settings.image_size_thumb.inner.y;
+                    var tw = self.settings.image_size_thumb.width;
+                    var th = self.settings.image_size_thumb.height;
+                    var tiw = self.settings.image_size_thumb.inner.width;
+                    var tih = self.settings.image_size_thumb.inner.height;
+                    
+                    Gm(tmp_file)
+                      .resize(tw, th)
+                      .crop(tiw, tih, (tw-tiw)/2, (th-tih)/2)
+                      .write(thumb_file, function(err) {
+                        
+                        Gm()
+                        .in('-page', '+0+0')
+                        .in(self.thumbFilename)
+                        .in('-page', '+'+ix+'+'+iy)
+                        .in(thumb_file)
+                        .in('-flatten')
+                        .write(thumb_file, function(err) {
+                          
+                          // Add text
+                          Gm(thumb_file)
+                            .font(self.settings.image_font)
+                            .fontSize(self.settings.image_size_thumb.text.username.font_size)
+                            .drawText(
+                              self.settings.image_size_thumb.text.username.x
+                              ,-(th/2) + self.settings.image_size_thumb.text.username.y
+                              ,img.user.username.toUpperCase(), 'center')
+                            .fontSize(self.settings.image_size_thumb.text.hashtag.font_size)
+                            .drawText(
+                              self.settings.image_size_thumb.text.hashtag.x
+                              ,-(th/2) + self.settings.image_size_thumb.text.hashtag.y
+                              ,self.settings.image_size_thumb.text.hashtag.text
+                              , 'center'
+                            )
+                            .write(thumb_file, function(err) {
+                            
+                              // Move image from tmp to save dir
+                              Fs.copy(tmp_file, dest_file, function() {
+
+                                // Remove tmp file
+                                Fs.unlink(tmp_file, function() {
+
+                                  // Add to images db
+                                  current_images.push(img.media_id);
+
+                                  // Update images db
+                                  Fs.outputJson(self.settings.images_db, current_images, function(err) {
+
+                                    self.log('Image ' + dest_file + ' saved');
+
+                                    // Next
+                                    setTimeout(function() {
+                                      
+                                      dl_img(queue);
+                                    }, self.settings.image_save_delay);
+                                  });
+                                });
+                              });
+                            });
+                            
+                        });
+                      });
+                  });
               }
               
             }, function() {
               
-              dl_img(queue);
+              setTimeout(function() {
+                dl_img(queue);
+              }, self.settings.image_save_delay);
             });
           } else {
             
-            dl_img(queue);
+            setTimeout(function() {
+              dl_img(queue);
+            }, self.settings.image_save_delay);
           }
         };
         
@@ -219,9 +369,11 @@ var ImageDownloader = new Class({
         
         
         setTimeout(function() {
+          
           self.lastModIdMin = 0;
           self.lastModIdMax = 0;
           self.start();
+          
         }, self.settings.request_delay);
         return;
       }
--- projects/admin/apps/instagram_admin/src/server.js
+++ projects/admin/apps/instagram_admin/src/server.js
@@ -25,27 +25,6 @@ var Server = new Class({
   
   // Options
   ,options: {
-    
-    "admin": {
-      "username": "",
-      "password": ""
-    },
-    "instagram": {
-      "client_id": ""
-      ,"client_secret": ""
-      ,"hashtags": []
-      ,"users": []
-      ,"request_delay": 2000
-    }
-    ,"http": {
-      "host": "localhost"
-      ,"port": 3333
-    }
-    ,"mongodb": {
-      "host": "localhost"
-      ,"port": 27017
-      ,"db": "mosaic"
-    }
   }
   
   // Constructor
@@ -57,9 +36,6 @@ var Server = new Class({
       .version('0.0.1')
       .option('-s, --settings [path]', 'Start with custom settings file')
       .option('-n, --nopolling', 'Start without media polling')
-      .option('-t, --polltags', 'Start with tag media polling')
-      .option('-u, --pollusers', 'Start with users media polling')
-      .option('-c, --clear', 'Clear all instagram subscriptions')
       .option('-d, --debug', 'Force debug console output')
       .parse(process.argv);
     
@@ -77,13 +53,12 @@ var Server = new Class({
       // Include project file
       var settings = require(file);
       
+      // Debug
       Console.DEBUG = settings.debug;
-      
       if(Program.debug) { Console.DEBUG = true; }
       
       Console.status('Started with settings ' + file);
-      
-      
+         
       // Set options
       this.setOptions(settings);
       
@@ -177,103 +152,16 @@ var Server = new Class({
       client_id: this.options.instagram.client_id
       ,client_secret: this.options.instagram.client_secret
     });
-    
-    // Clear subscriptions
-    if(Program.clear) {
-      
-      this.clearSubscriptions();
-      return;
-    }
-    
-    this.getUsers(function() {
-      
-      // Get recent media
-      if(!Program.nopolling) {
-        
-        if(!Program.polltags && !Program.pollusers) {
-          
-          // Get recent tag media
-          self.options.instagram.hashtags.each(function(tag, i) {
-            self.getTagRecentMedia(tag);
-          });
 
-          // Get recent user media
-          self.users.each(function(user, i) {
-           self.getUserRecentMedia(user);
-          });
-        
-        } else if(Program.polltags && !Program.pollusers) {
-          
-          // Get recent tag media
-          self.options.instagram.hashtags.each(function(tag, i) {
-            self.getTagRecentMedia(tag);
-          });
-        
-        } else if(!Program.polltags && Program.pollusers) {
-          
-          // Get recent user media
-          self.users.each(function(user, i) {
-            self.getUserRecentMedia(user);
-          });
-        }
-        
-      }
-      
-    });
+    // Get recent media
+    if(!Program.nopolling) {
 
-  }
-  
-  // Clear Instagram subscriptions
-  // --------------------------------------------------------
-  ,clearSubscriptions: function() {
-    
-    Console.status('Clear request send..please wait');
-
-    // Clear existing subscriptions
-    this.instagram.clearSubscriptions(function() {
-      
-      Console.info('All done!');
-      process.exit(0);
-    });
-
-  }
-  
-  // Get users
-  // --------------------------------------------------------
-  ,getUsers: function(callback) {
-    
-    var self = this;
-    this.users = new Array();
-
-    var get_user = function(queue, cb) {
-      
-      if(queue.length == 0) {
-        cb();
-        return;
-      }
-      
-      var username = queue.pop();
-      
-      self.instagram.getUser(username, function(err, users) {
-        
-        if(users) {
-          if(users.length > 0) {
-            self.users.push(users[0]);
-          } else {
-            Console.error('Could not find user ' + username); 
-          }
-
-          get_user(queue, cb);
-        } else {
-          Console.error('Could not find user ' + username); 
-          get_user(queue, cb);
-        }
-        
+      // Get recent tag media
+      self.options.instagram.hashtags.each(function(tag, i) {
+        self.getTagRecentMedia(tag);
       });
-    };
+    }
 
-    get_user(Array.clone(this.options.instagram.users), callback);
-    
   }
   
   // Get recent media
@@ -289,7 +177,7 @@ var Server = new Class({
       // if media is not updated or
       // failed to return any images
       var retry = function() {
-        
+       
         setTimeout(function() {
           var opt = {};
           
@@ -321,7 +209,7 @@ var Server = new Class({
         return;
       }
       
-     // Console.status('Received recent media for tag ' + tag);
+      Console.status('Received recent media for tag ' + tag);
       Console.status(remaining + ' remaining calls');
       
       // Empty check
@@ -352,8 +240,9 @@ var Server = new Class({
               // If media doesn't exist
               if(count==0) {
                 
-
-		var mtype = (self.options.instagram.users.indexOf(media.user.username) < 0) ? 'tag' : 'user';
+                // Check type (tag or user)
+		            var mtype = (self.options.instagram.users.indexOf(media.user.username) < 0) ? 'tag' : 'user';
+                
                 // Create media object
                 new_medias.push({
 
@@ -371,7 +260,7 @@ var Server = new Class({
                   ,modified_time: Date.now()
                   ,locked_time: Date.now()
                   ,locked: false
-                  ,approved: (mtype == 'user' && self.options.instagram.auto_approve_users) ? true : false
+                  ,approved: (mtype == 'user' && self.options.instagram.auto_approve_users)
                   ,reviewed: false
                 });
               }
@@ -379,6 +268,8 @@ var Server = new Class({
               next_media(list, callback);
               
             });
+          } else {
+            next_media(list, callback); 
           }
         };
         
@@ -388,6 +279,7 @@ var Server = new Class({
           
           if(new_medias.length > 0) {
             
+            // New images added
             Console.status(new_medias.length + ' images added for tag ' + tag);
             
             collection.insert(new_medias, {w:1}, function(err, result) {
@@ -408,8 +300,10 @@ var Server = new Class({
                 retry();
               }  
             });
+            
+          // No new images found..
           } else {
-           // Console.status('No new images found for tag ' + tag);
+            Console.status('No new images found for tag ' + tag);
             retry(); 
           }
         });
@@ -420,168 +314,6 @@ var Server = new Class({
     
     // Get recent media for tag
     this.instagram.getTagRecentMedia(tag, {}, rm_callback);
-  }
-  
-  // Get user recent media
-  // --------------------------------------------------------
-  ,getUserRecentMedia: function(user) {
-    
-    var self = this;
-   
-    // Get recent media for user callback
-    var rm_callback = function(err, medias, pagination, remaining, limit) {
-      
-      // Retry after x seconds
-      // if media is not updated or
-      // failed to return any images
-      var retry = function() {
-        
-        setTimeout(function() {
-          var opt = {};
-          
-          if(pagination) {
-            if(pagination.next_max_id) {
-              var opt = {
-                next_max_id: pagination.next_max_id
-              }; 
-            }
-          }
-          
-          self.instagram.getUserRecentMedia(user.id, opt, rm_callback);
-        }, self.options.instagram.request_delay); 
-      }
-      
-      // 503 error
-      if(err) {
-     
-        // Check for 503 status code
-        if(err.status_code == 503) {
-          Console.error('503 Service Unavailable. No server is available to handle this request. Possible cause is too ouch calls per hour or Instagram is having a hard time.');
-        } else {
-          Console.error('Get recent media request failed for ' + user.username + '. Make sure the profile is public!');
-        }
-        
-        // Retry after x seconds
-        retry();
-        
-        return;
-      }
-      
-     // Console.status('Received recent media for user ' + user.username);
-      Console.status(remaining + ' remaining calls');
-      
-      // Empty check
-      if(medias) {
-        
-        var collection = self.db.collection('instagram');
-  
-        // Fill new array with all media
-        var new_medias = new Array();
-        
-        // Iterator
-        var next_media = function(list, callback) {
-          
-          if(list.length == 0) {
-            callback();
-            return;
-          }
-          
-          var media = list.pop();
-          
-          // Check for image type, existing media id
-          if((media.type == 'image' && media.id)) {
-            
-            // Check if media has one of the set hashtags
-            var has_hashtag = false;
-            
-            media.tags.each(function(tag, i) {
-              if(self.options.instagram.hashtags.indexOf(tag) > -1) {
-                 has_hashtag = true;
-              }
-            });
-            
-            // If media has a corresponding hashtag
-            if(has_hashtag) {
-              
-              // Check if media already exists
-              var exists = collection.find({media_id: media.id}, {_id: 1}).limit(1);
-              exists.count(function(err, count) {
-
-                // If media doesn't exist
-                if(count == 0) {
-
-                  // Create media object
-                  new_medias.push({
-
-                    media_id: media.id
-                    ,msg_type: 'user'
-                    ,queue_id: ObjectID()
-                    ,images: media.images
-                    ,user: media.user
-                    ,filter: media.filter
-                    ,tags: media.tags
-                    ,link: media.link
-                    ,likes: media.likes.count
-                    ,location: media.location
-                    ,created_time: parseInt(media.created_time)
-                    ,modified_time: Date.now()
-                    ,locked_time: Date.now()
-                    ,locked: false
-                    ,approved: self.options.instagram.auto_approve_users
-                    ,reviewed: self.options.instagram.auto_approve_users
-                  });
-                }
-
-                next_media(list, callback);
-
-              });
-              
-            } else {
-              
-              // Next if tag is not in list
-              next_media(list, callback);
-            }
-          }
-        };
-        
-        // Check all media and add to db
-        // if media does not exist
-        next_media(Array.clone(medias), function() {
-    
-          if(new_medias.length > 0) {
-            
-            Console.status(new_medias.length + ' images added for user ' + user.username);
-            
-            collection.insert(new_medias, {w:1}, function(err, result) {
-
-              if(err) throw err;
-
-              // Call next page if pagination
-              // is available
-              if(pagination) { 
-                if(pagination.next) { 
-                 setTimeout(function() {
-			 pagination.next(rm_callback);
-			}, self.options.instagram.request_delay); 
-                } else { 
-                  retry(); 
-                }
-              } else {
-                retry();
-              }  
-            });
-          } else {
-           // Console.status('No new images found for user ' + user.username);
-            retry(); 
-          }
-        });
-      } else {
-        retry(); 
-      }
-    }
-    
-    // Get recent media for tag
-    this.instagram.getUserRecentMedia(user.id, {}, rm_callback);
   }
   
 });
