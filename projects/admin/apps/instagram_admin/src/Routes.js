bbd0247f7d431903d6fda6d977ef556020525d37
Martin Bartels <martin@apollomedia.nl>
2014-09-10 12:22:09 +0200

    mongo tests

--- projects/admin/apps/instagram_admin/src/Routes.js
+++ projects/admin/apps/instagram_admin/src/Routes.js
@@ -477,7 +477,7 @@ var Images = new Class({
           var result = collection.find({
             reviewed: true
             ,approved: true
-          }).sort({queue_id:-1}).limit(parseInt(req.params.limit));
+          ,hint:{queue_id:1}}).sort({queue_id:-1}).limit(parseInt(req.params.limit));
 
           result.toArray(function(err, docs) {
             
